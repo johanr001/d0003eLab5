7fe472536d8301a795cd0180db6566d8351c45a1
amadeusolofsson <coolaamadeus@gmail.com>
2025-03-04 19:26:59 +0100

    usart inits

--- Controller/Lab5/Lab5/avr_init.c
+++ Controller/Lab5/Lab5/avr_init.c
@@ -1,7 +1,9 @@
 #include "avr_init.h"
+#include <avr/io.h>
 #define FOSC 8000000// Clock Speed
 #define BAUD 9600
 #define MYUBRR FOSC/16/BAUD-1
+
 // avr_init() initierar klockan, timer1, prescalers etc.
 void avr_init(){
 	CLKPR = 0x80;
@@ -15,12 +17,12 @@ void avr_init(){
 	TIMSK1 |= (1 << OCIE1A);
 	OCR1A=194; //f_oCnA = (f_clk_I/O)/(2*N*(1+OCRnA) 194 eller 3905 för en sekund
 	
-	UBRR0H = (unsigned char)(UBRR>>8);
-	UBRR0L = (unsigned char)UBRR;
-	/* Enable receiver and transmitter */
-	UCSR0B = (1<<RXEN)|(1<<TXEN);
-	/* Set frame format: 8data, 2stop bit */
-	UCSR0C = (1<<USBS)|(3<<UCSZ0);
+	UBRR0H |= (MYUBRR>>8);
+	UBRR0L |= MYUBRR;
+	// Reciever, transmitter, och interrupt.
+	UCSR0B = (1 << RXEN0) | (1 << TXEN0) | (1 << RXCIE0);
+	// 8 Data, 1 stop bit
+	UCSR0C |= (1 << UCSZ01) | (3 << UCSZ00);
 	
 	
 };
