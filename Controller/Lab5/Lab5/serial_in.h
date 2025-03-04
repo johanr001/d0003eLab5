34b9cf53d6c8e3984c70e90d7f81e2f62809e33c
amadeusolofsson <coolaamadeus@gmail.com>
2025-03-04 18:17:37 +0100

    fixar headers för usart

--- Controller/Lab5/Lab5/serial_in.h
+++ Controller/Lab5/Lab5/serial_in.h
@@ -1,8 +1,15 @@
 #ifndef SERIAL_IN_H_
 #define SERIAL_IN_H_
 
+#include "TinyTimber.h"
+#include <avr/io.h>
+
+typedef struct {
+	Object super;
+} SerialIn;
 
 
+#define initSerialIn() { initObject()}
 
 
 #endif /* SERIAL_IN_H_ */
--- Controller/Lab5/Lab5/serial_out.h
+++ Controller/Lab5/Lab5/serial_out.h
@@ -1,8 +1,15 @@
 #ifndef SERIAL_OUT_H_
 #define SERIAL_OUT_H_
 
+#include "TinyTimber.h"
+#include <avr/io.h>
+
+typedef struct {
+	Object super;
+} SerialOut;
 
 
+#define initSerialOut() { initObject()}
 
 
 #endif /* SERIAL_OUT_H_ */
